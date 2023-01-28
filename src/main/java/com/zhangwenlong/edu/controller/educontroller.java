57de4f096d0f98185d6618e95fbff55c21a3010a
zhangwenlong <zhangwenlong@stoneoim.com>
2023-01-28 16:21:17 +0800

    zhangwenlong first commit

---
+++ src/main/java/com/zhangwenlong/edu/controller/educontroller.java
@@ -0,0 +1,23 @@
+package com.zhangwenlong.edu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/test")
+public class educontroller {
+//    @RequestMapping("/test")
+//    public Map<String, Object> teacher(){
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("test","test");
+//        map.put("test1","test1");
+//        map.put("test2","test2");
+//        map.put("test3","test3");
+//        map.put("test4","test4");
+//        return map;
+//    }
+
+}
